9bbf39e25c6d7dbd36082e6fe99ad4d406f6393d
junkyu <kuk941025@gmail.com>
2020-12-18 15:09:08 +0900

    Add css to textarea

--- packages/dodam-components/src/components/InputBox/InputBox.stories.tsx
+++ packages/dodam-components/src/components/InputBox/InputBox.stories.tsx
@@ -10,3 +10,11 @@ export default {
 const Template: Story<InputBoxProps> = (args) => <InputBox {...args} />;
 
 export const Default = Template.bind({});
+
+const TemplateMultiLine: Story<InputBoxProps> = (args) => <InputBox {...args} />;
+
+export const MultiLine = TemplateMultiLine.bind({});
+MultiLine.args = {
+  multiline: true,
+  placeholder: '동아리 소개 (200자 이상)',
+};
--- packages/dodam-components/src/components/InputBox/InputBox.tsx
+++ packages/dodam-components/src/components/InputBox/InputBox.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 
 export interface InputBoxProps extends React.InputHTMLAttributes<HTMLInputElement | HTMLTextAreaElement> {
   multiline?: boolean;
-  id?: string;
   label?: string;
 }
 
 const InputBox = React.forwardRef<HTMLInputElement, InputBoxProps>((props, ref) => {
   const { multiline, id, label, ...rest } = props;
   const Component = multiline ? 'textarea' : 'input';
-
   return (
     <>
       {label && (
@@ -18,6 +16,14 @@ const InputBox = React.forwardRef<HTMLInputElement, InputBoxProps>((props, ref) 
         </label>
       )}
       <Component id={id} className="form-control" ref={ref} {...rest} />
+
+      <style jsx>
+        {`
+          textarea {
+            min-height: 120px;
+          }
+        `}
+      </style>
     </>
   );
 });
