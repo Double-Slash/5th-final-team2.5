478eb23b2cec5728b64d0817d02611b11e6c77bb
junkyu <kuk941025@gmail.com>
2020-12-19 03:08:36 +0900

    Add toggle btn example

--- packages/dodam-components/src/components/Button/Button.stories.tsx
+++ packages/dodam-components/src/components/Button/Button.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { MouseEvent } from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 import Button, { ButtonProps } from './Button';
 
@@ -19,3 +19,36 @@ SecondaryFull.args = {
   variant: 'secondary',
   fullWidth: true,
 };
+
+const ToggleTemplate: Story<ButtonProps> = () => {
+  const [active, setActive] = React.useState('');
+
+  const handleClick = ({ target }: React.MouseEvent<HTMLButtonElement>) => {
+    const id = (target as HTMLButtonElement).id;
+    setActive(id);
+  };
+
+  return (
+    <>
+      <div className="row">
+        <Button onClick={handleClick} id="btn-1" outline active={'btn-1' === active}>
+          선택1
+        </Button>
+      </div>
+      <div className="row">
+        <Button onClick={handleClick} id="btn-2" outline active={'btn-2' === active}>
+          선택2
+        </Button>
+      </div>
+
+      <style jsx>
+        {`
+          .row {
+            margin: 8px 0;
+          }
+        `}
+      </style>
+    </>
+  );
+};
+export const ToggleButtons = ToggleTemplate.bind({});
