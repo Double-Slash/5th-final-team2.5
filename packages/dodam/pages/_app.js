be24ed2afcdb15bbaf7cfc6b79bfc09dc8f8622a
junkyu <kuk941025@gmail.com>
2020-12-20 20:45:22 +0900

    Fix eslint error

--- packages/dodam/pages/_app.js
+++ packages/dodam/pages/_app.js
@@ -1,25 +1,21 @@
 import '../styles/bootstrap.scss';
-import { Fragment } from 'react';
 import { Navigation } from '@dodam/components';
 
-function Dodam({ Component, pageProps }) {
-  console.log(Component);
-  return (
-    <Fragment>
-      <Navigation title="도담도담" />
-      <div className="dodam-root">
-        <Component {...pageProps} />
-      </div>
+const Dodam = ({ Component, pageProps }) => (
+  <>
+    <Navigation title="도담도담" />
+    <div className="dodam-root">
+      <Component {...pageProps} />
+    </div>
 
-      <style jsx>
-        {`
-          .dodam-root {
-            padding: 58px 8px 8px 8px;
-          }
-        `}
-      </style>
-    </Fragment>
-  );
-}
+    <style jsx>
+      {`
+        .dodam-root {
+          padding: 58px 8px 8px 8px;
+        }
+      `}
+    </style>
+  </>
+);
 
 export default Dodam;
