6f2234d23337deaa6e83bab8137d89b15b008d2d
junkyu <kuk941025@gmail.com>
2020-12-20 14:58:09 +0900

    Add test: checking default type of input

--- packages/dodam-components/src/components/File/File.test.js
+++ packages/dodam-components/src/components/File/File.test.js
@@ -10,4 +10,10 @@ describe('<File />', () => {
     expect(wrapper.find('button').length).toBe(1);
     expect(wrapper.find('label').length).toBe(0);
   });
+
+  it('input의 type은 File로 설정된다.', () => {
+    const component = mount(<File />).find('input');
+
+    expect(component.props().type).toBe('file');
+  });
 });
