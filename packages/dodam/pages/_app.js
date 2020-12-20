0f1fd347320ddaf7839a49eb5a959a3f82be584e
junkyu <kuk941025@gmail.com>
2020-12-20 20:29:23 +0900

    Add navigation bar as default ui

--- packages/dodam/pages/_app.js
+++ packages/dodam/pages/_app.js
@@ -1,6 +1,25 @@
 import '../styles/bootstrap.scss';
+import { Fragment } from 'react';
+import { Navigation } from '@dodam/components';
+
 function Dodam({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  console.log(Component);
+  return (
+    <Fragment>
+      <Navigation title="도담도담" />
+      <div className="dodam-root">
+        <Component {...pageProps} />
+      </div>
+
+      <style jsx>
+        {`
+          .dodam-root {
+            padding: 58px 8px 8px 8px;
+          }
+        `}
+      </style>
+    </Fragment>
+  );
 }
 
 export default Dodam;
--- packages/dodam/pages/index.js
+++ packages/dodam/pages/index.js
@@ -2,18 +2,9 @@ import { InputBox, Button } from '@dodam/components';
 
 export default function Home() {
   return (
-    <div className="title">
-      <div className="container">
-        <InputBox type="text" placeholder="placeholder text" />
-        <Button>Click</Button>
-      </div>
-      <style jsx>
-        {`
-          .container {
-            padding: 8px;
-          }
-        `}
-      </style>
+    <div>
+      <InputBox type="text" placeholder="placeholder text" />
+      <Button>Click</Button>
     </div>
   );
 }
