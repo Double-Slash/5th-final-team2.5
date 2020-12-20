07d8b26bd9153f60e92ec55dc5aeaf560db51a3e
junkyu <kuk941025@gmail.com>
2020-12-20 14:27:13 +0900

    Handles bootstrap sass

--- packages/dodam-components/.storybook/main.js
+++ packages/dodam-components/.storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: ['@storybook/preset-scss', '@storybook/addon-links', '@storybook/addon-essentials'],
 };
--- packages/dodam-components/src/index.ts
+++ packages/dodam-components/src/index.ts
@@ -1,4 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import './styles/custom.scss';
 import 'react-datepicker/dist/react-datepicker.css';
 
 export { default as Button } from './components/Button';
