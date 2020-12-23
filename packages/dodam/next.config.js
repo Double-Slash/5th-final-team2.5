b32d63044a9bbe5eaaae4b6944a65cc1f767a1e1
junkyu <kuk941025@gmail.com>
2020-12-23 18:00:53 +0900

    Remove comment

--- packages/dodam/next.config.js
+++ packages/dodam/next.config.js
@@ -1,9 +1,3 @@
 const withTM = require('next-transpile-modules')(['@dodam/components'], true);
 
-// const webpack = withTypescript(
-//   withTM({
-//     transpileModules: ['dodam-components'],
-//   })
-// );
-
 module.exports = withTM();
