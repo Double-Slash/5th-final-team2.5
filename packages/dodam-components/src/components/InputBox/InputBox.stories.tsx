49572d5dd68526a9e9f125a79ee6ea0d43dfdefc
junkyu <kuk941025@gmail.com>
2020-12-23 21:28:37 +0900

    Fix inputbox props

--- packages/dodam-components/src/components/InputBox/InputBox.stories.tsx
+++ packages/dodam-components/src/components/InputBox/InputBox.stories.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 import InputBox, { InputBoxProps } from './InputBox';
-import { InputProps } from '../Input/Input';
 
 export default {
   title: 'InputBox',
