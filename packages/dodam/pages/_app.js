79da607824fb7ebfff1a03c30feb38fc2b7c03da
junkyu <kuk941025@gmail.com>
2020-12-27 17:50:13 +0900

    Add animations on search page

--- packages/dodam/pages/_app.js
+++ packages/dodam/pages/_app.js
@@ -13,6 +13,7 @@ const Dodam = ({ Component, pageProps }) => (
       {`
         .dodam-root {
           padding: 58px 8px 8px 8px;
+          min-height: 100vh;
         }
       `}
     </style>
