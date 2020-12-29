d5a8bb82db9f0f5e3dae91b7cbfa23be573a8009
jjmmll0727 <jjmmll0727@gmail.com>
2020-12-29 17:37:38 +0900

    sso login 미완성

--- packages/dodam-server/routes/user/index.js
+++ packages/dodam-server/routes/user/index.js
@@ -4,5 +4,7 @@ const userController = require('../../controller/userController');
 
 router.post('/signup', userController.signup);
 router.post('/login', userController.login);
+router.post('/login/google', userController.login_google);
+router.post('/login/google/callback', userController.login_google_callback);
 
 module.exports = router;
