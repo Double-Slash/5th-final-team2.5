0718eb43d51e48bdcf9d6f5bf92658d670d58c9e
jjmmll0727 <jjmmll0727@gmail.com>
2021-01-02 01:10:22 +0900

    club db setting / todo: image file, sso

---
+++ packages/dodam-server/models/Clubs.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CommentaireSchema = new Schema({
+  commentaire: [
+    {
+      type: String,
+    }
+  ]
+})
+
+const ClubSchema = new Schema({
+  writer: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  school: {
+    type: String,
+    required: true,
+  },
+  field: {
+    type: String,
+    required: true,
+  },
+  area: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: Number,
+    required: true,
+  },
+  recruit_start_date: {
+    type: String,
+  },
+  recruit_end_date: {
+    type: String,
+  },
+  represent_content: {
+    type: String,
+  },
+  introduce_content: {
+    type: String,
+  },
+  comment: [
+    CommentaireSchema
+  ],
+  likes: {
+    type: Number,
+  },
+  register_date: {
+    type: Date,
+    default: Date.now(),
+  }
+});
+module.exports = mongoose.model('club', UserSchema);
