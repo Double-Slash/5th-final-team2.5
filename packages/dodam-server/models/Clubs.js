b5a7a458dcf9713efb2e428777bdf1e4b30ea686
jjmmll0727 <jjmmll0727@gmail.com>
2021-01-02 22:50:54 +0900

    require type url

--- packages/dodam-server/models/Clubs.js
+++ packages/dodam-server/models/Clubs.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+require('mongoose-type-url');
 
 const CommentaireSchema = new Schema({
   Commentaire: [
